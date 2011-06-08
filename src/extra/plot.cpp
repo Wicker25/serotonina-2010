299af666a266cf4a4540c52cbbe37f88fb66d462
Wicker25 <wicker25@gmail.com>
2011-06-08 13:13:04 +0200

    Release 1.0.4: Fixed license

--- src/extra/plot.cpp
+++ src/extra/plot.cpp
@@ -5,19 +5,18 @@
 
 	This file is part of Serotonina.
 
-	This library is free software; you can redistribute it and/or modify
-	it under the terms of the GNU Lesser General Public License as published by
-	the Free Software Foundation; either version 2 of the License, or
-	(at your option) any later version.
+    Serotonina is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-	This library is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
-	GNU Lesser General Public License for more details.
+    Serotonina is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-	You should have received a copy of the GNU Lesser General Public License
-	along with this library; if not, write to the Free Software
-	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA	02110-1301	USA
+    You should have received a copy of the GNU General Public License
+    along with Serotonina.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef _SEROTONINA_PLOT_CPP_
